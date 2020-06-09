a06b91e3e89fe177fd3743df7a9533218c5d3572
Scott <seharlan@gmail.com>
2020-06-09 12:47:57 -0700

    all sorts in js and ts done

--- Deck copy.ts
+++ Deck copy.ts
@@ -18,32 +18,87 @@ class Deck {
     }, [])
     return this.deck
   }
-  sortByValue = () => {
-    const valueSortedDeck = []
-    const deckCopy = [...this.deck]
-    let lowestValue = 15
+  sortByValue() {
+    const valueSortedDeck = [];
+    const deckCopy = [...this.deck];
+    let lowestValue = 15;
     //find the lowest value
     deckCopy.forEach((card) => {
-      if (card.value < lowestValue) lowestValue = card.value
-    })
+      if(card.value < lowestValue) lowestValue = card.value;
+    });
 
     //remove items from deckCopy only when the value matches the lowestValue, if none are found increase lowestValue
     while(deckCopy.length) {
-      const cardToAddIndex = deckCopy.findIndex(card => card.value === lowestValue)
+      const cardToAddIndex = deckCopy.findIndex(card => card.value === lowestValue);
 
       if(cardToAddIndex === -1) lowestValue++;
-      else valueSortedDeck.push(deckCopy.splice(cardToAddIndex, 1)[0])
+      else valueSortedDeck.push(deckCopy.splice(cardToAddIndex, 1)[0]);
     }
-    return valueSortedDeck
+    return valueSortedDeck;
   }
   
-  sortBySuit = () => {
+  sortBySuit() {
+    const suitSortedDeck = [];
+    const deckCopy = [...this.deck];
+    
+    const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+    let suitIndex = 0;
+
+    while(deckCopy.length) {
+      const cardToAddIndex = deckCopy.findIndex(card => card.suit === suits[suitIndex]);
+
+      if(cardToAddIndex === -1) suitIndex++;
+      else suitSortedDeck.push(deckCopy.splice(cardToAddIndex, 1)[0]);
+    }
+    return suitSortedDeck;
+  }
+  sortByValueThenSuit() {
+    const valueSuitSortedDeck = [];
+    const deckCopy = [...this.deck];
+    let lowestValue = 15;
+    deckCopy.forEach((card) => {
+      if(card.value < lowestValue) lowestValue = card.value;
+    });
+
+    const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+    let suitIndex = 0;
+
+    while(deckCopy.length) {
+      const cardToAddIndex = deckCopy.findIndex(card => (card.value === lowestValue && card.suit === suits[suitIndex]));
+
+      if(cardToAddIndex === -1 && suitIndex < 4) suitIndex++;
+      else if(cardToAddIndex === -1) {
+        lowestValue++; 
+        suitIndex = 0;
+      }
+      else valueSuitSortedDeck.push(deckCopy.splice(cardToAddIndex, 1)[0]);
+    }
+    return valueSuitSortedDeck;
 
   }
-  sortByValueThenSuit = () => {
+  sortBySuitThenValue() {
+    const suitValueSortedDeck = [];
+    const deckCopy = [...this.deck];
 
-  }
-  sortBySuitThenValue = () => {
+    const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+    let suitIndex = 0;
+
+    while(deckCopy.length) {
+      let lowestValue = 15;
+      deckCopy.forEach((card) => {
+        if(card.value < lowestValue && card.suit === suits[suitIndex]) lowestValue = card.value;
+      });
+
+      const cardToAddIndex = deckCopy.findIndex(card => (card.value === lowestValue && card.suit === suits[suitIndex]));
+
+      if(cardToAddIndex === -1 && lowestValue < 15) lowestValue++;
+      if(cardToAddIndex === -1) {
+        suitIndex++;
+        lowestValue = 15;
+      } 
+      else suitValueSortedDeck.push(deckCopy.splice(cardToAddIndex, 1)[0]);
+    }
+    return suitValueSortedDeck;
 
   }
 };
--- Deck.test.js
+++ Deck.test.js
@@ -7,26 +7,62 @@ describe('deck-sort-challenge', () => {
     const suits = ['clubs', 'diamonds', 'hearts', 'spades', 'clubs', 'clubs', 'diamonds'];
     const values = [5, 7, 2, 4, 5, 6, 2];
     const expected = [
-      {suit: 'clubs', value: 5},
-      {suit: 'diamonds', value: 7},
-      {suit: 'hearts', value: 2},
-      {suit: 'spades', value: 4},
-      {suit: 'clubs', value: 5},
-      {suit: 'clubs', value: 6},
-      {suit: 'diamonds', value: 2},
+      { suit: 'clubs', value: 5 },
+      { suit: 'diamonds', value: 7 },
+      { suit: 'hearts', value: 2 },
+      { suit: 'spades', value: 4 },
+      { suit: 'clubs', value: 5 },
+      { suit: 'clubs', value: 6 },
+      { suit: 'diamonds', value: 2 },
     ];
     expect(deck.feed(suits, values)).toEqual(expected);
   });
   it('sorts by value', () => {
     const expected = [
-      {suit: 'hearts', value: 2},
-      {suit: 'diamonds', value: 2},
-      {suit: 'spades', value: 4},
-      {suit: 'clubs', value: 5},
-      {suit: 'clubs', value: 5},
-      {suit: 'clubs', value: 6},
-      {suit: 'diamonds', value: 7},
-    ]
-    expect(deck.sortByValue()).toEqual(expected)
-  })
+      { suit: 'hearts', value: 2 },
+      { suit: 'diamonds', value: 2 },
+      { suit: 'spades', value: 4 },
+      { suit: 'clubs', value: 5 },
+      { suit: 'clubs', value: 5 },
+      { suit: 'clubs', value: 6 },
+      { suit: 'diamonds', value: 7 },
+    ];
+    expect(deck.sortByValue()).toEqual(expected);
+  });
+  it('sorts by suit', () => {
+    const expected = [
+      { suit: 'clubs', value: 5 },
+      { suit: 'clubs', value: 5 },
+      { suit: 'clubs', value: 6 },
+      { suit: 'diamonds', value: 7 },
+      { suit: 'diamonds', value: 2 },
+      { suit: 'hearts', value: 2 },
+      { suit: 'spades', value: 4 },
+    ];
+    expect(deck.sortBySuit()).toEqual(expected);
+  });
+  it('sorts by value then suit', () => {
+    const expected = [
+      { suit: 'diamonds', value: 2 },
+      { suit: 'hearts', value: 2 },
+      { suit: 'spades', value: 4 },
+      { suit: 'clubs', value: 5 },
+      { suit: 'clubs', value: 5 },
+      { suit: 'clubs', value: 6 },
+      { suit: 'diamonds', value: 7 },
+    ];
+    expect(deck.sortByValueThenSuit()).toEqual(expected);
+  });
+  it('sorts by suit then value', () => {
+    const expected = [
+      { suit: 'clubs', value: 5 },
+      { suit: 'clubs', value: 5 },
+      { suit: 'clubs', value: 6 },
+      { suit: 'diamonds', value: 2 },
+      { suit: 'diamonds', value: 7 },
+      { suit: 'hearts', value: 2 },
+      { suit: 'spades', value: 4 },
+    ];
+    expect(deck.sortBySuitThenValue()).toEqual(expected);
+  });
 });
